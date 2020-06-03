c8978f69dcb6bd11272588dc7f1f29bd77000685
Alhadis <gardnerjohng@gmail.com>
2020-06-03 16:35:00 +1000

    Update man-db to 2.9.2

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -1,20 +1,28 @@
 class ManDb < Formula
 	desc "Modern, featureful implementation of the Unix man page system"
 	homepage "https://nongnu.org/man-db/"
-	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.9.1.tar.xz"
-	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.9.1.tar.xz"
-	sha256 "ba3d8afc5c09a7265a8dabfa0e7c1f4b3ab97df9abf1f6810faa8f301056c74f"
+	head "https://git.savannah.gnu.org/git/man-db.git"
+	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.9.2.tar.xz"
+	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.9.2.tar.xz"
+	sha256 "7b383f26c67525205f96a4a3bfb27993396992794adda6fe05ad565f369f047c"
 
 	depends_on "libpipeline"
+	depends_on "gettext"
+	depends_on "zstd" => :optional
 
 	def install
-		system "./configure",
-			"--prefix=#{prefix}",
-			"--with-systemdtmpfilesdir=no",
-			"--with-systemdsystemunitdir=no",
-			"--disable-cache-owner",
-			"--disable-setuid"
-
+		args = %W[
+			--prefix=#{prefix}
+			--with-systemdtmpfilesdir=no
+			--with-systemdsystemunitdir=no
+			--disable-dependency-tracking
+			--disable-silent-rules
+			--disable-cache-owner
+			--disable-setuid
+			--disable-shared
+			--enable-static
+		]
+		system "./configure", *args
 		system "make"
 		system "make", "install"
 	end
