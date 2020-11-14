61dc6261c49d45164f1e5827088e7b737df5536e
Alhadis <gardnerjohng@gmail.com>
2020-11-15 00:11:16 +1100

    Update heirloom-doctools to 191015

--- Formula/heirloom-doctools.rb
+++ Formula/heirloom-doctools.rb
@@ -1,15 +1,13 @@
 class HeirloomDoctools < Formula
 	desc "Portable, heavily-enhanced versions of troff and related tools"
 	homepage "https://n-t-roff.github.io/heirloom/doctools.html"
-	url "https://github.com/n-t-roff/heirloom-doctools/releases/download/160308/heirloom-doctools-160308.tar.bz2"
-	sha256 "e4aeae0e5303537755c04226c06d98a46aa35913d1a179708fbc368f93731a26"
+	url "https://github.com/n-t-roff/heirloom-doctools/releases/download/191015/heirloom-doctools-191015.tar.bz2"
+	sha256 "a169912358874ecb396c6ce02d08144db00f0e65b9de528216d5a9addea8a34a"
 	head "https://github.com/n-t-roff/heirloom-doctools.git"
-
-	# TODO: Remove this once a new release is cut
-	patch :DATA unless build.head?
 
 	def install
 		args = %W[
+			--print-directory
 			PREFIX=#{prefix}
 			BINDIR=#{libexec}/bin
 			MANDIR=#{libexec}/man
@@ -20,7 +18,6 @@ class HeirloomDoctools < Formula
 			TABDIR=#{pkgshare}/nterm
 			HYPDIR=#{pkgshare}/hyphen
 		]
-		inreplace "makefile", %r{^\ttroff/troff\.d/dhtml \\$\K}ms, "\n\ttroff/troff.d/devaps \\"
 		system "./configure"
 		system "make", *args
 		system "make", "install", *args
@@ -31,7 +28,6 @@ class HeirloomDoctools < Formula
 			dir = File.basename(File.dirname page)
 			(man/dir).install_symlink page => "heirloom-#{File.basename page}"
 		end
-		
 		doc.install Dir["doc/*"]
 		mkdir_p doc/"examples", verbose: true
 		(doc/"examples").install Dir["stuff/{footnotes.tr,demo/*}"]
@@ -52,92 +48,11 @@ class HeirloomDoctools < Formula
 			If you want the unprefixed versions, add the following to your shell's
 			startup file:
 			
-				export PATH="#{opt_prefix}/libexec/bin:$PATH"
-				export MANPATH="#{opt_prefix}/libexec/man:$MANPATH"
+				export PATH="\x1B[4m#{opt_prefix}/libexec/bin\x1B[24m:$PATH"
+				export MANPATH="\x1B[4m#{opt_prefix}/libexec/man\x1B[24m:$MANPATH"
 			
-			Additional documentation and examples can be found in #{pkgshare}.
+			Additional documentation and examples can be found in
+			\x1B[4m#{pkgshare}\x1B[24m.
 		EOF
 	end
 end
-
-__END__
-diff --git a/eqn/eqn.d/Makefile.mk b/eqn/eqn.d/Makefile.mk
-index d6e573c..020c6fe 100644
---- a/eqn/eqn.d/Makefile.mk
-+++ b/eqn/eqn.d/Makefile.mk
-@@ -34,23 +34,23 @@ clean:
- mrproper: clean
-
- diacrit.o: ../diacrit.c ../e.h y.tab.h
--eqnbox.o: ../eqnbox.c ../e.h
--font.o: ../font.c ../e.h
--fromto.o: ../fromto.c ../e.h
-+eqnbox.o: ../eqnbox.c ../e.h y.tab.h
-+font.o: ../font.c ../e.h y.tab.h
-+fromto.o: ../fromto.c ../e.h y.tab.h
- funny.o: ../funny.c ../e.h y.tab.h
- glob.o: ../glob.c ../e.h
- integral.o: ../integral.c ../e.h y.tab.h
- io.o: ../io.c ../e.h
- lex.o: ../lex.c ../e.h y.tab.h
- lookup.o: ../lookup.c ../e.h y.tab.h
--mark.o: ../mark.c ../e.h
--matrix.o: ../matrix.c ../e.h
--move.o: ../move.c ../e.h y.tab.h
--over.o: ../over.c ../e.h
--paren.o: ../paren.c ../e.h
--pile.o: ../pile.c ../e.h
--shift.o: ../shift.c ../e.h y.tab.h
--size.o: ../size.c ../e.h
--sqrt.o: ../sqrt.c ../e.h
-+mark.o: ../mark.c ../e.h y.tab.h
-+matrix.o: ../matrix.c ../e.h y.tab.h
-+move.o: ../move.c ../e.h y.tab.h
-+over.o: ../over.c ../e.h y.tab.h
-+paren.o: ../paren.c ../e.h y.tab.h
-+pile.o: ../pile.c ../e.h y.tab.h
-+shift.o: ../shift.c ../e.h y.tab.h
-+size.o: ../size.c ../e.h y.tab.h
-+sqrt.o: ../sqrt.c ../e.h y.tab.h
- text.o: ../text.c ../e.h y.tab.h
- e.o: e.c ../e.h
-diff --git a/eqn/neqn.d/Makefile.mk b/eqn/neqn.d/Makefile.mk
-index bb924fc..33b6649 100644
---- a/eqn/neqn.d/Makefile.mk
-+++ b/eqn/neqn.d/Makefile.mk
-@@ -31,23 +31,23 @@ clean:
- mrproper: clean
- 
- diacrit.o: ../diacrit.c ../e.h y.tab.h
--eqnbox.o: ../eqnbox.c ../e.h
--font.o: ../font.c ../e.h
--fromto.o: ../fromto.c ../e.h
-+eqnbox.o: ../eqnbox.c ../e.h y.tab.h
-+font.o: ../font.c ../e.h y.tab.h
-+fromto.o: ../fromto.c ../e.h y.tab.h
- funny.o: ../funny.c ../e.h y.tab.h
- glob.o: ../glob.c ../e.h
- integral.o: ../integral.c ../e.h y.tab.h
- io.o: ../io.c ../e.h
- lex.o: ../lex.c ../e.h y.tab.h
- lookup.o: ../lookup.c ../e.h y.tab.h
--mark.o: ../mark.c ../e.h
--matrix.o: ../matrix.c ../e.h
--move.o: ../move.c ../e.h y.tab.h
--over.o: ../over.c ../e.h
--paren.o: ../paren.c ../e.h
--pile.o: ../pile.c ../e.h
--shift.o: ../shift.c ../e.h y.tab.h
--size.o: ../size.c ../e.h
--sqrt.o: ../sqrt.c ../e.h
-+mark.o: ../mark.c ../e.h y.tab.h
-+matrix.o: ../matrix.c ../e.h y.tab.h
-+move.o: ../move.c ../e.h y.tab.h
-+over.o: ../over.c ../e.h y.tab.h
-+paren.o: ../paren.c ../e.h y.tab.h
-+pile.o: ../pile.c ../e.h y.tab.h
-+shift.o: ../shift.c ../e.h y.tab.h
-+size.o: ../size.c ../e.h y.tab.h
-+sqrt.o: ../sqrt.c ../e.h y.tab.h
- text.o: ../text.c ../e.h y.tab.h
- e.o: e.c ../e.h
