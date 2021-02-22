329335d8b4910910d39efa03285793ea67db8175
Alhadis <gardnerjohng@gmail.com>
2021-02-22 12:06:47 +1100

    Update man-db to 2.9.4

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -2,11 +2,10 @@ class ManDb < Formula
 	desc "Modern, featureful implementation of the Unix man page system"
 	homepage "https://nongnu.org/man-db/"
 	head "https://git.savannah.gnu.org/git/man-db.git"
-	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.9.3.tar.xz"
-	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.9.3.tar.xz"
-	sha256 "fa5aa11ab0692daf737e76947f45669225db310b2801a5911bceb7551c5597b8"
+	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.9.4.tar.xz"
+	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.9.4.tar.xz"
+	sha256 "b66c99edfad16ad928c889f87cf76380263c1609323c280b3a9e6963fdb16756"
 	license "GPL-2.0-or-later"
-	revision 1
 
 	depends_on "libpipeline"
 	depends_on "gettext"
