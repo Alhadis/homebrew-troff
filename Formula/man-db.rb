ac85144c349d439c0094ee0c8e3ad29ddb24569b
Alhadis <gardnerjohng@gmail.com>
2022-02-07 00:22:42 +1100

    Update man-db to 2.10.0

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -1,11 +1,12 @@
 class ManDb < Formula
 	desc "Modern, featureful implementation of the Unix man page system"
 	homepage "https://nongnu.org/man-db/"
-	head "https://git.savannah.gnu.org/git/man-db.git"
-	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.9.4.tar.xz"
-	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.9.4.tar.xz"
-	sha256 "b66c99edfad16ad928c889f87cf76380263c1609323c280b3a9e6963fdb16756"
+	head "https://gitlab.com/cjwatson/man-db.git"
+	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.10.0.tar.xz"
+	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.10.0.tar.xz"
+	sha256 "0a8629022f7117dc7fc6473c6fdb14913b24b106059bb056abee87dbd6070c79"
 	license "GPL-2.0-or-later"
+	patch :DATA # TODO: Remove once 2.10.1 is released
 
 	depends_on "libpipeline"
 	depends_on "gettext"
@@ -30,6 +31,7 @@ class ManDb < Formula
 			--disable-shared
 			--enable-static
 		]
+		ENV.append_to_cflags "-std=c99"
 		system "./configure", *args
 		system "make"
 		system "make", "install"
@@ -42,3 +44,34 @@ class ManDb < Formula
 		assert_match "The true utility always returns with exit code zero", output
 	end
 end
+
+__END__
+diff --git a/src/man.c b/src/man.c
+index c91abf15..5d5caaf1 100644
+--- a/src/man.c
++++ b/src/man.c
+@@ -383,15 +383,18 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
+ 
+ 		case OPT_WARNINGS:
+ #ifdef NROFF_WARNINGS
+-			char *s = xstrdup (arg ? arg : default_roff_warnings);
+-			const char *warning;
++			{
++				char *s = xstrdup
++					(arg ? arg : default_roff_warnings);
++				const char *warning;
+ 
+-			for (warning = strtok (s, ","); warning;
+-			     warning = strtok (NULL, ","))
+-				gl_list_add_last (roff_warnings,
+-						  xstrdup (warning));
++				for (warning = strtok (s, ","); warning;
++				     warning = strtok (NULL, ","))
++					gl_list_add_last (roff_warnings,
++							  xstrdup (warning));
+ 
+-			free (s);
++				free (s);
++			}
+ #endif /* NROFF_WARNINGS */
+ 			return 0;
