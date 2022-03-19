d7874f8915828d1b3512f439fc4550b1cada8cdb
Alhadis <gardnerjohng@gmail.com>
2022-03-20 04:40:16 +1100

    Fix building man-db(1) formula from `--HEAD`

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -1,7 +1,6 @@
 class ManDb < Formula
 	desc "Modern, featureful implementation of the Unix man page system"
 	homepage "https://nongnu.org/man-db/"
-	head "https://gitlab.com/cjwatson/man-db.git"
 	url "https://download.savannah.nongnu.org/releases/man-db/man-db-2.10.2.tar.xz"
 	mirror "https://download-mirror.savannah.gnu.org/releases/man-db/man-db-2.10.2.tar.xz"
 	sha256 "ee97954d492a13731903c9d0727b9b01e5089edbd695f0cdb58d405a5af5514d"
@@ -12,6 +11,13 @@ class ManDb < Formula
 	depends_on "zstd" => :optional
 	depends_on "groff"
 	uses_from_macos "zlib"
+
+	head do
+		url "https://gitlab.com/cjwatson/man-db.git", branch: "main"
+		depends_on "autoconf" => :build
+		depends_on "automake" => :build
+		depends_on "libtool"  => :build
+	end
 
 	livecheck do
 		url "https://download.savannah.gnu.org/releases/man-db/"
@@ -31,6 +37,7 @@ class ManDb < Formula
 			--enable-static
 		]
 		ENV.append_to_cflags "-std=c99"
+		system "./bootstrap" if build.head?
 		system "./configure", *args
 		system "make"
 		system "make", "install"
@@ -40,6 +47,6 @@ class ManDb < Formula
 		ENV["PAGER"] = "cat"
 		output = shell_output("#{bin}/man true")
 		assert_match "BSD General Commands Manual", output
-		assert_match "The true utility always returns with exit code zero", output
+		assert_match /\A(?:TRUE|true)\(1\)[ \t]/, output
 	end
 end
